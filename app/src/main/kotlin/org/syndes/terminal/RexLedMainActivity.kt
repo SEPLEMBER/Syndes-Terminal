0e596b6ece69927813235dc0991f99a2e32bf854
SEPLEMBER <216141135+SEPLEMBER@users.noreply.github.com>
2026-01-07 17:02:19 +0700

    Create RexLedMainActivity.kt

---
+++ app/src/main/kotlin/org/syndes/terminal/RexLedMainActivity.kt
@@ -0,0 +1,12 @@
+package org.syndes.terminal
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RexLedMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rexled)
+    }
+}
